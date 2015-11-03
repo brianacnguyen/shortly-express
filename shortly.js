e8221639b42cdf6e43a76d3d62bc7abc8ba8747e
Brian Nguyen <brianacnguyen@gmail.com>
2015-11-03 12:31:52 -0800

    completed basic requirements

--- shortly.js
+++ shortly.js
@@ -2,7 +2,7 @@ var express = require('express');
 var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
-
+var bcrypt = require('bcrypt-nodejs');
 
 var db = require('./app/config');
 var Users = require('./app/collections/users');
@@ -13,6 +13,14 @@ var Click = require('./app/models/click');
 
 var app = express();
 
+var session = require('express-session');
+
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'shhhh, very secret'
+}));
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(partials());
@@ -22,37 +30,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
-
 app.get('/', 
 function(req, res) {
-  if(//req of a login token )
-  res.render('index');
-else {
-  //redirect to login 
-  //use res.redirect
-}
+  if(util.checkUser(req)) {
+    res.render('index');
+  }
+  else {
+    res.redirect('login');
+  }
 });
 
 app.get('/create', 
 function(req, res) {
-  if(//req of a login token )
-  res.render('index');
-else {
-  //redirect to login 
-  //use res.redirect
-}
+  if(util.checkUser(req)) {
+    res.render('index');
+  }
+  else {
+    res.redirect('login');
+  }
 });
 
 app.get('/links', 
 function(req, res) {
-  if(//req of a login token )
-  Links.reset().fetch().then(function(links) {
-    res.send(200, links.models);
-  });
-else {
-  //redirect to login 
-  //use res.redirect
-}
+  if(util.checkUser(req)) {
+    Links.reset().fetch().then(function(links) {
+      res.send(200, links.models);
+    });
+  }
+  else {
+    res.redirect('login');
+  }
 });
 
 app.post('/links', 
@@ -92,7 +99,55 @@ function(req, res) {
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
+app.get('/login', function (req, res) {
+  res.render('login');
+});
+app.get('/signup', function (req, res) {
+  res.render('signup');
+});
+//req.session.loggedIn = true when authenticated 
+ app.post('/login', function (req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+  new User({ username: username }).fetch().then(function(user){
+    if(!user){
+      res.redirect('/login');
+    }
+    else { 
+      bcrypt.compare(password, user.get('password'), function(err, matched){
+        if(matched){
+          req.session.loggedIn = true; 
+          res.redirect('/');
+        }else{
+          res.redirect('/login');
+        }
+      });
+    }
+  });
+ });
+app.post('/signup', function (req,res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  new User({ username: username }).fetch().then(function(user){
+    if (!user) {
+      //create account
+      bcrypt.hash(password, null, null, function(err, hash){
+        Users.create({
+          username: username,
+          password: hash
+        }).then(function(user){
+          req.session.loggedIn = true; 
+          res.redirect('/');
+        });
+      });
+    }
+    else {
+      console.log('Pick a different username');
+      res.redirect('/signup');
+    }
+  });
 
+})
 
 
 /************************************************************/
